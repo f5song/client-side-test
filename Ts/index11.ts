0bdbc6e121cbf1a5d7cec07d28ee4b751d17b547
f5song <folkkykytak@gmail.com>
2025-01-21 14:01:13 +0700

    typescript

---
+++ Ts/index11.ts
@@ -0,0 +1,7 @@
+declare module 'str-utils' {
+    export function strReverse(input: string): string;
+    export function strToLower(input: string): string;
+    export function strToUpper(input: string): string;
+    export function strRandomize(input: string): string;
+    export function strInvertCase(input: string): string;
+}
